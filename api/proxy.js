98fcfbc7aee4f781d0289dabd4abc7991db63073
SidimaD <Sidihassan17@gmail.com>
2026-05-10 04:28:37 +0100

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,15 +1,11 @@
 const fetch = require('node-fetch');
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+export const config = { api: { bodyParser: false } };
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', '*');
 
   if (req.method === 'OPTIONS') return res.status(200).end();
 
@@ -21,21 +17,16 @@ module.exports = async (req, res) => {
     for await (const chunk of req) { chunks.push(chunk); }
     const rawBody = Buffer.concat(chunks);
 
-    const forwardHeaders = { ...req.headers };
-    delete forwardHeaders.host;
-    delete forwardHeaders.connection;
-    delete forwardHeaders['content-length'];
-
     const response = await fetch(target, {
       method: req.method,
-      headers: forwardHeaders,
-      body: req.method !== 'GET' && req.method !== 'HEAD' ? rawBody : undefined
+      headers: { ...req.headers, host: new URL(target).host },
+      body: req.method !== 'GET' ? rawBody : undefined
     });
 
     res.status(response.status);
     const buffer = await response.buffer();
     res.send(buffer);
-  } catch (err) {
-    res.status(500).send('Proxy Error: ' + err.message);
+  } catch (e) {
+    res.status(500).send(e.message);
   }
 };
